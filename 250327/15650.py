bd1629319dd18b9fe444c723a01367318f9da057
smj0503 <wovj0503@gmail.com>
2025-03-27 18:22:08 +0900

    [2025.03.25] Day 7

--- 250327/15650.py
+++ 250327/15650.py
@@ -1,3 +1,21 @@
 # BOJ 15650
 
+N, M = map(int, input().split())
+ans = []
+visited = [0] * (N + 1)
 
+def dfs():
+    if len(ans) == M:
+        print(*ans)
+        return
+
+    for i in range(1, N + 1):
+        if not visited[i]:
+            if len(ans) == 0 or i > ans[-1]:
+                visited[i] = 1
+                ans.append(i)
+                dfs()
+                visited[i] = 0
+                ans.pop()
+
+dfs()
