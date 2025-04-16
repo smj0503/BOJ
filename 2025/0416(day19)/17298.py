ed9382e4fd3db129156cf7001a2bb1fd63868616
smj0503 <wovj0503@gmail.com>
2025-04-16 16:38:06 +0900

    [2025.04.16] Day 19

---
+++ 2025/0416(day19)/17298.py
@@ -0,0 +1,15 @@
+# BOJ 17298
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+answer = [-1] * n
+stack = []
+
+for i in range(n):
+    while stack and arr[stack[-1]] < arr[i]:
+        idx = stack.pop()
+        answer[idx] = arr[i]
+    stack.append(i)
+
+print(*answer)
