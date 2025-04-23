34c85b870e37e729140b46f8215183b85dd166b6
smj0503 <wovj0503@gmail.com>
2025-04-23 10:22:10 +0900

    [2025.04.23] Day 21

---
+++ 2025/0423(day21)/1717.py
@@ -0,0 +1,34 @@
+# BOJ 1717
+
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+parent = [-1] * (n+1)
+
+def find(x):
+    if parent[x] < 0:
+        return x
+    parent[x] = find(parent[x])
+    return parent[x]
+
+def union(x, y):
+    x = find(x)
+    y = find(y)
+
+    if x == y:
+        return False
+
+    parent[y] = x
+    return True
+
+for _ in range(m):
+    op, a, b = map(int, input().split())
+    if op == 0:
+        union(a, b)
+    else:
+        if find(a) == find(b):
+            print('YES')
+        else:
+            print('NO')
