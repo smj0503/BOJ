7bfa7a4d76483b0025b02c642887bdf5d1c6807a
smj0503 <wovj0503@gmail.com>
2025-05-02 15:33:07 +0900

    [2025.05.02] Day 25

--- 2025/0502(day25)/17609_2.py
+++ 2025/0502(day25)/17609_2.py
@@ -1,3 +1,27 @@
 # BOJ 17609
 
+def is_palindrome(s):
+    return s == s[::-1]
 
+def is_pseudo_palindrome(s):
+    left, right = 0, len(s)-1
+
+    while left < right:
+        if s[left] != s[right]:
+            temp1 = s[left+1:right+1]
+            temp2 = s[left:right]
+            return is_palindrome(temp1) or is_palindrome(temp2)
+        left += 1
+        right -= 1
+
+    return True
+
+t = int(input())
+for _ in range(t):
+    string = input()
+    if is_palindrome(string):
+        print(0)
+    elif is_pseudo_palindrome(string):
+        print(1)
+    else:
+        print(2)
