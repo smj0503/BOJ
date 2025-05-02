1708ea46655854f36a969b9ad85d19c3664b4e22
smj0503 <wovj0503@gmail.com>
2025-05-02 23:40:11 +0900

    [2025.05.02] Day 25

--- 2025/0502(day25)/11724.py
+++ 2025/0502(day25)/11724.py
@@ -1,1 +1,32 @@
 # BOJ 11724
+
+from collections import deque
+import sys
+input = sys.stdin.readline
+
+n, m = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+visited = [0] * (n+1)
+ans = 0
+
+for _ in range(m):
+    a, b = map(int, input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+def bfs(start):
+    q = deque()
+    q.append(start)
+
+    while q:
+        x = q.popleft()
+        for nx in graph[x]:
+            if not visited[nx]:
+                visited[nx] = 1
+                q.append(nx)
+
+for i in range(1, n+1):
+    if not visited[i]:
+        bfs(i)
+        ans += 1
+print(ans)
